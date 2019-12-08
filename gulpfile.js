74d3a3da827d0a00caeda146627d0f00739a6775
이소윤 <s_lee77176@fanshaweonline.ca>
2019-12-08 17:43:09 -0500

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+
+const gulp = require("gulp");
+const sass = require ("gulp-sass");
+const postcss = require("gulp-postcss");
+const nano = require("cssnano");
+
+
+function compile(done) {
+    gulp.src("sass/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(postcss([nano()]))
+    .pipe(gulp.dest("public/css"))
+    done()
+}
+
+
+exports.compile = compile;
